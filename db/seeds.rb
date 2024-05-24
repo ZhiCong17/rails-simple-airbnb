167c98068de7efd62c06317cbb10d14c97f95384
ZhiCong Ore <zhicong1989@gmail.com>
2024-05-24 23:32:51 +0800

    created flat model, seeds, controller and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+london_flat1 = {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area,
+  large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+}
+
+london_flat2 = {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area,
+  large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+}
+
+london_flat3 = {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area,
+  large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+}
+
+london_flat4 = {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area,
+  large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+}
+
+[london_flat1, london_flat2, london_flat3, london_flat4].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
