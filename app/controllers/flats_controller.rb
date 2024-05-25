9ada6cdacb08df0c114261716003501f7453015d
ZhiCong Ore <zhicong1989@gmail.com>
2024-05-25 18:58:53 +0800

    added destroy, edit and update

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,6 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit update destroy]
+
   def index
     @flats = Flat.all
   end
@@ -18,12 +20,31 @@ class FlatsController < ApplicationController
   end
 
   def show
+  end
 
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
